befa930ce943593a849812575703d41237c6cd0c
Paul Annesley <paul@annesley.cc>
2014-11-18 23:23:51 -0800

    Minimal s3_loader.

---
+++ thumbor_botornado/s3_loader.py
@@ -0,0 +1,10 @@
+from botornado.s3.bucket import AsyncBucket
+from botornado.s3.connection import AsyncS3Connection
+from botornado.s3.key import AsyncKey
+
+def load(context, url, callback):
+    bucket_name, object_name = url.split("/", 1)
+    connection = AsyncS3Connection() # load credentials from environment
+    bucket = AsyncBucket(connection, bucket_name)
+    key = AsyncKey(bucket, object_name)
+    key.read(callback=callback)
