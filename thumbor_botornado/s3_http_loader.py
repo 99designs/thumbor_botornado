26c8a4a6ba7a2ca01c6cd92804d95e964aa31d58
Paul Annesley <paul@annesley.cc>
2015-01-05 12:16:47 -0800

    s3_http_loader: four-spaces, not tabs.

--- thumbor_botornado/s3_http_loader.py
+++ thumbor_botornado/s3_http_loader.py
@@ -3,7 +3,7 @@ import thumbor.loaders.http_loader as HttpLoader
 import re
 
 def load(context, url, callback):
-	if re.match('https?:', url, re.IGNORECASE):
-		HttpLoader.load(context, url, callback)
-	else:
-		S3Loader.load(context, url, callback)
+    if re.match('https?:', url, re.IGNORECASE):
+        HttpLoader.load(context, url, callback)
+    else:
+        S3Loader.load(context, url, callback)
