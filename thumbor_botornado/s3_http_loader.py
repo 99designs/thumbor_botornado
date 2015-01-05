9c26ee20618636edb7f7cacddbd6603028f3000c
Paul Annesley <paul@annesley.cc>
2015-01-05 12:20:38 -0800

    s3_http_loader: HTTP_RE compiled as module constant.

--- thumbor_botornado/s3_http_loader.py
+++ thumbor_botornado/s3_http_loader.py
@@ -2,8 +2,10 @@ import thumbor_botornado.s3_loader as S3Loader
 import thumbor.loaders.http_loader as HttpLoader
 import re
 
+HTTP_RE = re.compile('https?:', re.IGNORECASE)
+
 def load(context, url, callback):
-    if re.match('https?:', url, re.IGNORECASE):
+    if HTTP_RE.match(url):
         HttpLoader.load(context, url, callback)
     else:
         S3Loader.load(context, url, callback)
