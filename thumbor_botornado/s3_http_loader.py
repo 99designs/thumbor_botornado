f008ae789b39960974a4415af2f95995de0e9bee
Paul Annesley <paul@annesley.cc>
2015-07-21 01:01:15 -0700

    s3_http_loader uses return_future for Thumbor 5 compatibility.
    Should have been included in 48176f9.

--- thumbor_botornado/s3_http_loader.py
+++ thumbor_botornado/s3_http_loader.py
@@ -1,9 +1,11 @@
+from tornado.concurrent import return_future
 import thumbor_botornado.s3_loader as S3Loader
 import thumbor.loaders.http_loader as HttpLoader
 import re
 
 HTTP_RE = re.compile(r'\Ahttps?:', re.IGNORECASE)
 
+@return_future
 def load(context, url, callback):
     if HTTP_RE.match(url):
         HttpLoader.load(context, url, callback)
